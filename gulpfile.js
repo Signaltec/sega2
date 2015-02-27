6e258ac2a2ecb16c901d82ebebeaeeec0c6e9bb0
ildarka <ildarka@rfc1918.private.ip.localhost>
2015-02-27 18:55:13 +0300

    move dev to list

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     webserver = require('gulp-webserver'),
+    symlink = require('gulp-symlink'),
     less = require('gulp-less');
 
 var server = {
@@ -7,23 +8,26 @@ var server = {
   port: '3000'
 }
 
-var path = { src: "src/", dst: "dist/" };
+var path = { src: "src/", dst: "dist/sega2" };
 
 gulp.task('copy', function () {
-  gulp.src([path.src +'*.html']).pipe(gulp.dest(path.dst));
+  gulp.src([path.src +'*.html']).pipe(gulp.dest(path.dst)).pipe(gulp.dest('./'));
+
+  // Symlinks for development (flat structure)
+  //gulp.src('bower_components/**')
+  //.pipe(symlink.relative('../$'));
 });
 
 gulp.task('less', function () {
   gulp.src([path.src + 'sega2.less'])
     .pipe(less())
     .pipe(gulp.dest(path.dst))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
-
   gulp.watch([path.src + '*.less'], ['less']);
   gulp.watch([path.src + '*.html'], ['copy']);
-  
 });
 
 gulp.task('webserver', function() {
